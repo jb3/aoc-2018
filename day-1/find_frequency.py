c3a53170c1996bc3604aa39f8d103cd5e0e39d2a
Joseph Banks <joseph@josephbanks.me>
2018-12-01 18:39:00 +0000

    Day 1

---
+++ day-1/find_frequency.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import itertools
+
+with open("input.txt", "r") as f:
+    inputs = f.readlines()
+    inputs = [int(inp.strip()) for inp in inputs]
+
+frequency = 0
+
+seen_freqs = set()
+
+for number in inputs:
+    frequency += number
+    seen_freqs.add(frequency)
+
+    
+for number in itertools.cycle(inputs):
+    frequency += number
+    if frequency in seen_freqs:
+        print("found")
+        break
+
+print(frequency)
