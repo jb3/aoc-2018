483359394143dba5286714a6bead1c8bd7810254
Joseph Banks <joseph@josephbanks.me>
2018-12-08 13:23:24 +0000

    Somehow I messed up day 6

--- day-6/chronal_coordinates.py
+++ day-6/chronal_coordinates.py
@@ -29,12 +29,12 @@ points = []
 for coordinate in coordinates:
     points.append(Point2D.from_text(coordinate))
 
-sorted_y = sorted(points, lambda point: point.y)
+sorted_y = sorted(points, key=lambda point: point.y)
 
 min_y = sorted_y[0].y
 max_y = sorted_y[::-1][0].y
 
-sorted_x = sorted(points, lambda point: point.x)
+sorted_x = sorted(points, key=lambda point: point.x)
 
 min_x = sorted_x[0].x
 max_x = sorted_x[::-1][0].x
@@ -42,8 +42,7 @@ max_x = sorted_x[::-1][0].x
 grid_points = {point: 0 for point in points}
 
 
-for x, y in itertools.product(range(min_x, max_x + 1),
-                              range(min_y, max_y + 1)):
+for x, y in itertools.product(range(min_x, max_x + 1), range(min_y, max_y + 1)):
     point = Point2D(x, y)
 
     closest_point = map(lambda p: (p, p.distance_from(point)), points)
@@ -62,9 +61,9 @@ for x, y in itertools.product(range(min_x, max_x + 1),
 
     grid_points[sorted_points[0][0]] += 1
 
-aoc_part_1_answer = sorted(grid_points.items(),
-                           key=lambda point: point[1],
-                           reverse=True)[0][1]
+aoc_part_1_answer = sorted(
+    grid_points.items(), key=lambda point: point[1], reverse=True
+)[0][1]
 
 print(f"AoC part 1 answer: {aoc_part_1_answer}")
 
